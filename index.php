19d638b109f801a75be95d8857938766b4a0dba4
Antonio69999 <antoine.chabeauti@gmail.com>
2023-06-27 16:03:48 +0200

    php_part2

---
+++ index.php
@@ -0,0 +1,124 @@
+<?php
+
+//Exercice_1
+// $age = 18;
+
+// if ($age >=18) {
+//     echo"Majeur";
+// }
+
+// else {
+//     echo "Mineur";
+// }
+
+//Exercice_2
+
+// $isEasy = 1;
+
+// if ($isEasy == true) {
+//     echo "It's easy!!";
+// }
+
+// else {
+//     echo "It's hard!!";
+// }
+
+//Exercice_3
+
+// $age = 12;
+// $gender = 'femme';
+
+// if ($gender == 'homme' && $age >= 18) {
+//     echo ("Vous êtes un homme et vous êtes majeur");
+// }
+// elseif ($gender == 'homme' && $age < 18) {
+//     echo ("Vous êtes un homme et vous êtes mineur");
+// }
+
+// if ($gender == 'femme' && $age >= 18) {
+//     echo ("Vous êtes un femme et vous êtes majeur");
+// }
+// elseif ($gender == 'femme' && $age < 18) {
+//     echo ("Vous êtes un femme et vous êtes mineur");
+// }
+
+
+//Exerice_4
+
+// $magnitude = 6;
+
+// switch ($magnitude) {
+//     case 1:
+//         echo "Micro imp à ress";
+//         break;
+    
+//     case 2:
+//         echo "blabla2";
+//         break;
+    
+//     case '3':
+//         echo "blabla3";
+//         break;
+    
+//     case '4':
+//         echo "blabla4";
+//         break;
+
+//     case '5':
+//         echo "blabla5";
+//         break;
+    
+//     case '6':
+//         echo "blabla6";
+//         break;
+// }
+
+// Exercice_5
+
+// $gender = "Homme";
+
+// if ($gender != 'Homme') {
+//     echo "C'est une développeuse !!!";
+// }
+
+// else {
+//     echo "C'est un développeur";
+// }
+
+//Exercice_6
+
+// $age = 16; 
+
+// if ($age >= 18) {
+//     echo "Tu es majeur";
+// }
+// else {
+//     echo "Tu n'est pas majeur";
+// }
+
+//Exercice_7
+
+// $isOk = false;
+
+// if ($isOk == false) {
+//     echo "'s not good";
+// }
+
+// else {
+//     echo "'s good";
+// }
+
+
+//Exercice_8
+
+$isOk=1;
+
+if ($isOk) {
+    echo ("c'est ok");
+}
+
+else {
+    echo ("c'est pas bon");
+}
+
+?>
